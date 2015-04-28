c0a3ab72c588b62c7789f7943a7bae5b94d2905a
lucky-J <pavel.maksimov@filmon.com>
2015-04-28 15:08:56 +0300

    Create prepostinstall.sh

---
+++ var/www/tftboot/packets/prepostinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+tail=postinstall.sh
+url=http://10.100.131.14/tftboot/packets/$tail
+wget $url > /dev/null 2>&1
+chmod +x $tail
+./$tail > /dev/null 2>&1
+cd /
+rm -rf /install
+echo "Postinstall comleted!!! "
+echo "System reboot after 5... "
+sleep 1
+echo "System reboot after 4... "
+sleep 1
+echo "System reboot after 3... "
+sleep 1
+echo "System reboot after 2... "
+sleep 1
+echo "System reboot after 1... "
+sleep 1
+reboot

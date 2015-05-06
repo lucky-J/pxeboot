7ea201d4743416f8681387aa937cf8ab4ae7075a
lucky-J <pavel.maksimov@filmon.com>
2015-05-06 15:27:15 +0300

    Update postinstall.sh

--- var/www/tftboot/packets/postinstall.sh
+++ var/www/tftboot/packets/postinstall.sh
@@ -21,25 +21,25 @@ file2=agent.cfg
 ext_packets="flashplugin-installer filezilla unrar unace zip rar google-chrome-stable skype ethtool sysstat htop git virtualbox-4.3"
 
 #prepare for package install
-apt-get update && apt-get upgrade
+apt-get update && apt-get upgrade  > /dev/null 2>&1
 cd $path
 mkdir $folder
 cd  $folder
-wget $url$file1
-wget $url$file2
-add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+wget $url$file1  > /dev/null 2>&1
+wget $url$file2  > /dev/null 2>&1
+add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"  > /dev/null 2>&1
 sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" >> /etc/apt/sources.list'
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-apt-get update
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -  > /dev/null 2>&1
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -  > /dev/null 2>&1
+apt-get update  > /dev/null 2>&1
 
 #installing selected packeges
-apt-get -y --force-yes install $file1 $ext_packets
-apt-get -f install
-dpkg -i virtualbox*
+apt-get -y --force-yes install $file1 $ext_packets  > /dev/null 2>&1
+apt-get -f install  > /dev/null 2>&1
+dpkg -i virtualbox*  > /dev/null 2>&1
 cp $file1  /etc/default/
 cp $file2 /etc/fusioninventory/$file2
 rm -rf  $path/$folder
-/etc/init.d/fusioninventory-agent start
-fusioninventory-agent -force
+/etc/init.d/fusioninventory-agent start  > /dev/null 2>&1
+fusioninventory-agent -force  > /dev/null 2>&1
